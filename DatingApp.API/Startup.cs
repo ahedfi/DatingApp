05414b80d0efa20f7cd20c15789af3f38bb7c7cf
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2019-10-28 22:36:21 +0100

    Add AuthController

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
             options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
