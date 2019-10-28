71161465d364c9e742267d7448bbeb04a6e1841b
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2019-10-28 21:47:12 +0100

    Add AuthRepository

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -5,7 +5,8 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-      public DataContext(DbContextOptions options):base(options){}
-      public DbSet<Value> Values { get; set; }
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
