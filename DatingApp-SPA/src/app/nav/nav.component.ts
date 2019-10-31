c5894f4e547e6376b348ff14714c53604f424724
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2019-10-31 08:40:53 +0100

    use login service to store user token in a local storage

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../_services/auth.service";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.css"]
 })
 export class NavComponent implements OnInit {
+  model: any = {};
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login() {
+    debugger
+    this.authService.login(this.model).subscribe(
+      result => {
+        console.log("Loggged in Successfully");
+      },
+      error => {
+        console.log("Failed to login");
+      }
+    );
   }
-
 }
