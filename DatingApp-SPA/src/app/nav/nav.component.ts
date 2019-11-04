f3bf291515dcb540b68cd86a14321be5183924f9
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2019-11-04 22:43:08 +0100

    Add @auth0/angular-jwt library

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -27,8 +27,7 @@ export class NavComponent implements OnInit {
     );
   }
   isLoggedIn() {
-    var token = localStorage.getItem("token");
-    return !!token;
+    return this.authService.loggedIn();
   }
   logout() {
     localStorage.removeItem("token");
