e28b4cee6cf34adb808aef612457621b7144b7be
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2019-11-23 11:30:58 +0100

    add member detail component

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MemberListComponent } from "./members/member-list/member-list.component
 import { MessagesComponent } from "./messages/messages.component";
 import { ListsComponent } from "./lists/lists.component";
 import { AuthGuard } from "./_guards/auth.guard";
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,10 @@ const routes: Routes = [
       {
         path: "members",
         component: MemberListComponent
+      },
+      {
+        path: "members/:id",
+        component: MemberDetailComponent
       },
       {
         path: "messages",
